809c8c378e0310963a5fad2572ec302adc3926ff
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-10-01 11:02:12 +0100

    Init

---
+++ src/PivotPay.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Pivot;
+
+/**
+ * @author Zubs <zubairidrisaweda@gmail.com>
+ * @package PivotPayPHP
+ */
+class PivotPay
+{
+    public function __construct()
+    {
+        echo "Hello World";
+    }
+}
---
+++ test/PivotPayTest.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once '../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class PivotPayTest extends TestCase
+{
+    public function testPivotPay()
+    {
+        $this->assertTrue(true);
+    }
+}
